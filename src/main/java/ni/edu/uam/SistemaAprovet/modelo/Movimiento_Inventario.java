ef11d5eabb80b650101d42174f0ea0f1c96f52c8
gague-uam <ggpaiz@uamv.edu.ni>
2025-11-26 11:59:23 -0600

    Se agrego la clase Movimiento_Inventario, al igual que la clase Inventario

--- src/main/java/ni/edu/uam/SistemaAprovet/modelo/Movimiento_Inventario.java
+++ src/main/java/ni/edu/uam/SistemaAprovet/modelo/Movimiento_Inventario.java
@@ -1,7 +1,41 @@
 package ni.edu.uam.SistemaAprovet.modelo;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.Hidden;
+import org.openxava.annotations.Required;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter @Setter
 public class Movimiento_Inventario {
 
+    @Id
+    @Hidden
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String oid;
 
+    @Column(nullable = false)
+    @Required(message = "La fecha es obligatoria")
+    private LocalDate fecha;
 
+    @Column(nullable = false)
+    @Required(message = "La cantidad es obligatoria")
+    private Integer cantidad;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @Required(message = "Debe indicar el producto")
+    private Producto producto;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @Required(message = "Debe indicar el tipo de movimiento")
+    private Tipo_Movimiento ref_tipo;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @Required(message = "Debe indicar el inventario afectado")
+    private Inventario inventario;
 }
